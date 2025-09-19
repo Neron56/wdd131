f2828aec692a9e53dadaca573870ee55eb198434
Neron56 <Michaelshaw20004@gmail.com>
2025-09-18 18:15:39 -0600

    second push for audit

--- scripts/place.js
+++ scripts/place.js
@@ -2,8 +2,11 @@ const currentyear = document.querySelector("#currentyear");
 const today = new Date();
 const windChill = document.querySelector("#windChill")
 currentyear.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
-let windSpeed = 14.4;
+let windSpeed = 34.5;
 let temperature = 10;
 function calculateWindChill(windSpeed, temperature){
-    chill = 
+  return (temperature <= 10 && windSpeed > 4.8) 
+  ? Math.round((13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16)) * 10) / 10
+  : temperature;
 }
+windChill.innerHTML = `<span>${calculateWindChill(windSpeed, temperature)} &#8451;</span>`;
